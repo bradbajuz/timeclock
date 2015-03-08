ca50a198bac262bf93cf6d415c8d4c3e894d6092
Brad Ballard <brad@bradballard.com>
2015-03-08 17:23:20 -0400

    Setup boostrap modal for new view

--- app/controllers/time_data_controller.rb
+++ app/controllers/time_data_controller.rb
@@ -1,10 +1,20 @@
 class TimeDataController < ApplicationController
+  respond_to :html, :js
 
   def index
+    @time_datum = TimeDatum.new
     @time_data = TimeDatum.all
   end
 
   def new
     @time_datum = TimeDatum.new
   end
+
+  def create
+    @time_datum = TimeDatum.new(time_datum_params)
+  end
+end
+
+def time_datum_params
+  params.require(:time_datum).permit(:clock_in, :clock_out, :break_out, :break_in, :notes)
 end
