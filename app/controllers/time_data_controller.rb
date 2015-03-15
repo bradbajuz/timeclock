f69a014852d34124d32eb04f747e5db16fdad81c
Brad Ballard <brad@bradballard.com>
2015-03-14 21:52:10 -0400

    Fix double posting after save

--- app/controllers/time_data_controller.rb
+++ app/controllers/time_data_controller.rb
@@ -6,15 +6,24 @@ class TimeDataController < ApplicationController
     @time_data = TimeDatum.all
   end
 
-  def new
-    @time_datum = TimeDatum.new
-  end
-
   def create
     @time_datum = TimeDatum.new(time_datum_params)
-  end
-end
 
-def time_datum_params
-  params.require(:time_datum).permit(:clock_in, :clock_out, :break_out, :break_in, :notes)
+    if @time_datum.save
+      flash[:notice] = "Time was saved successfully."
+      redirect_to time_data_path
+      return
+    else
+      flash[:error] = "Error saving time. Please try again."
+    end
+
+    respond_with(@time_datum) do |f|
+      f.html { redirect_to time_data_path }
+    end
+  end
+
+private
+  def time_datum_params
+    params.require(:time_datum).permit(:clock_in, :clock_out, :break_out, :break_in, :notes)
+  end
 end
