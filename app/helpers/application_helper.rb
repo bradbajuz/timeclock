fd961739e4760396715ceecbaae46c24a282fc62
Brad Ballard <brad@bradballard.com>
2015-03-28 23:04:29 -0400

    Get form time formatting set correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def time_format(datetime)
+    datetime.strftime("%l:%M %p") unless datetime.blank?
+  end
+
 end
+
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:time_format] = "%l:%M %p"
+Time::DATE_FORMATS[:day_format] =  "%A: %B, %d %Y"
